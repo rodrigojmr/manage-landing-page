09ff142fb56962798318472a8f3be4fce6863258
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-10-25 22:05:00 +0000

    First commit - Header done, doing graphic animations

---
+++ index.js
@@ -0,0 +1,14 @@
+gsap.fromTo('#bar-top-1', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo(
+  '#bar-top-2',
+  { scaleY: 0, zIndex: 1 },
+  { duration: 1.5, scaleY: 0.4 }
+);
+gsap.fromTo('#bar-top-3', { scaleY: 0 }, { duration: 1.5, scaleY: 0.5 });
+gsap.fromTo('#bar-bottom-1', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo('#bar-bottom-2', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo('#bar-bottom-3', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo('#bar-bottom-4', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo('#bar-bottom-5', { scaleY: 0 }, { duration: 1.5, scaleY: 0.9 });
+gsap.fromTo('#bar-bottom-6', { scaleY: 0 }, { duration: 1.5, scaleY: 0.4 });
+gsap.fromTo('#bar-bottom-7', { scaleY: 0 }, { duration: 1.5, scaleY: 0.5 });
