ce297c3b5e1c63a25cf1eeb25005be7f29af9036
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-10-31 21:02:49 +0000

    Hero section graph

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-gsap.fromTo('#bar-top-1', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
+gsap.fromTo('#bar-top-1', { height: 0 }, { duration: 1.5, height: '50%' });
+gsap.fromTo('#bar-top-2', { height: 0 }, { duration: 1.5, height: '100%' });
 gsap.fromTo(
-  '#bar-top-2',
-  { scaleY: 0, zIndex: 1 },
-  { duration: 1.5, scaleY: 0.4 }
+  '#bar-top-3',
+  { height: 0, zIndex: 1 },
+  { duration: 1.5, height: '80%' }
 );
-gsap.fromTo('#bar-top-3', { scaleY: 0 }, { duration: 1.5, scaleY: 0.5 });
-gsap.fromTo('#bar-bottom-1', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
-gsap.fromTo('#bar-bottom-2', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
-gsap.fromTo('#bar-bottom-3', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
-gsap.fromTo('#bar-bottom-4', { scaleY: 0 }, { duration: 1.5, scaleY: 0.2 });
-gsap.fromTo('#bar-bottom-5', { scaleY: 0 }, { duration: 1.5, scaleY: 0.9 });
-gsap.fromTo('#bar-bottom-6', { scaleY: 0 }, { duration: 1.5, scaleY: 0.4 });
-gsap.fromTo('#bar-bottom-7', { scaleY: 0 }, { duration: 1.5, scaleY: 0.5 });
+
+gsap.fromTo(
+  '#bar-bottom-1',
+  { height: 0, zIndex: 1 },
+  { duration: 1.5, height: '50%' }
+);
+gsap.fromTo('#bar-bottom-2', { height: 0 }, { duration: 1.5, height: '60%' });
+gsap.fromTo('#bar-bottom-3', { height: 0 }, { duration: 1.5, height: '35%' });
+gsap.fromTo(
+  '#bar-bottom-4',
+  { height: 0, zIndex: 1 },
+  { duration: 1.5, height: '60%' }
+);
+gsap.fromTo('#bar-bottom-5', { height: 0 }, { duration: 1.5, height: '100%' });
+gsap.fromTo(
+  '#bar-bottom-6',
+  { height: 0, zIndex: 1 },
+  { duration: 1.5, height: '65%' }
+);
+gsap.fromTo('#bar-bottom-7', { height: 0 }, { duration: 1.5, height: '80%' });
