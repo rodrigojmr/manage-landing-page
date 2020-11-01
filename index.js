1977d5097402c598146fee52ec41b727202c6732
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-11-01 20:49:28 +0000

    Early testimonial version

--- index.js
+++ index.js
@@ -25,3 +25,30 @@ gsap.fromTo(
   { duration: 1.5, height: '65%' }
 );
 gsap.fromTo('#bar-bottom-7', { height: 0 }, { duration: 1.5, height: '80%' });
+
+let el = document.querySelector('.testimonial');
+console.log('el: ', el);
+let x = 0,
+  y = 0,
+  top = 0,
+  left = 0;
+
+let draggingFunction = e => {
+  document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', draggingFunction);
+  });
+
+  el.scrollLeft = left - e.pageX + x;
+  el.scrollTop = top - e.pageY + y;
+};
+
+el.addEventListener('mousedown', e => {
+  e.preventDefault();
+
+  y = e.pageY;
+  x = e.pageX;
+  top = el.scrollTop;
+  left = el.scrollLeft;
+
+  document.addEventListener('mousemove', draggingFunction);
+});
